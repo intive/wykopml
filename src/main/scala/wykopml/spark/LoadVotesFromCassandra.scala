9fed137c68c777867f6dbc6f2ea4a4652aaa0280
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-11 14:51:47 +0100

    LoadVotesFromCassandra using Spark

---
+++ src/main/scala/wykopml/spark/LoadVotesFromCassandra.scala
@@ -0,0 +1,42 @@
+package wykopml.spark
+
+import java.time.{ZoneId, LocalDateTime}
+
+import com.datastax.driver.core.Row
+import com.datastax.spark.connector.{ColumnName, ColumnRef}
+import com.datastax.spark.connector.cql.TableDef
+import com.datastax.spark.connector.rdd.reader.{RowReader, RowReaderFactory}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import wykopml.bo.Vote
+import wykopml.storage.WykopDatabase
+import com.datastax.spark.connector._
+
+case object LoadVotesFromCassandra {
+
+  def apply(sc: SparkContext): RDD[Vote] = {
+    implicit val rowReaderFactory = new RowReaderFactory[Vote] {
+      override def rowReader(table: TableDef, selectedColumns: IndexedSeq[ColumnRef]): RowReader[Vote] = new RowReader[Vote] {
+        override def neededColumns: Option[Seq[ColumnRef]] = Some(
+          Seq("wykopid", "who", "isup", "when").map(ColumnName(_))
+        )
+
+        override def read(row: Row, columnNames: Array[String]): Vote = {
+          Vote(
+            row.getLong("wykopid").toInt,
+            row.getString("who"),
+            row.getBool("isup"),
+            LocalDateTime.ofInstant(row.getTimestamp("when").toInstant, ZoneId.systemDefault())
+          )
+
+        }
+      }
+
+      override def targetClass: Class[Vote] = classOf[Vote]
+    }
+
+    sc.cassandraTable[Vote](WykopDatabase.keyspace.name, WykopDatabase.votes.tableName)
+
+  }
+
+}
